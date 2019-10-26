6e3a0a2a7d4cfd297461b4f7cb8b07a0df4ffdcc
Elisabeth Loeser <19419418+ElliLoes@users.noreply.github.com>
2019-10-26 17:54:07 +1100

    Ui

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,37 @@
 var friends = [
     {
         name: "Pippy Pippa",
-        photo: "https://www.canva.com/photos/MADFFIKemZk-smiling-woman/",
+        photo: "https://marketplace.canva.com/MADGyQrzV2I/4/screen_2x/canva-brown-and-white-american-pit-bull-terrier-with-brown-costume-MADGyQrzV2I.jpg",
         scores: [1, 3, 5, 3,4 ,4, 2, 3, 1, 2]
     },
     {
         name: "Dewey",
-        photo: "https://images.pexels.com/photos/1024311/pexels-photo-1024311.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        photo: "https://marketplace.canva.com/MADGv7Jn8tk/7/screen_2x/canva-close-up-photography-of-cat-MADGv7Jn8tk.jpg",
         scores: [4, 1, 5, 4, 3, 2, 2, 4, 1, 2]
     },
     {
         name: "Cinderella",
-        photo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        photo: "https://marketplace.canva.com/MADGyCuP-GE/4/screen_2x/canva-closeup-photo-of-adult-white-and-tan-american-bulldog-near-wall-MADGyCuP-GE.jpg",
         scores: [1, 3, 1, 3, 4, 2, 3, 3, 5, 4]
     },
     {
         name: "Aladin",
-        photo: "https://images.pexels.com/photos/346796/pexels-photo-346796.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        photo: "https://marketplace.canva.com/MADGxk4riFs/4/screen_2x/canva-close-up-of-black-dog-MADGxk4riFs.jpg",
         scores: [2, 2, 4, 3, 5, 4, 2, 3, 1, 2]
     },
     {
         name: "Aiden",
-        photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        photo: "https://marketplace.canva.com/MADGybfBTkc/4/screen_2x/canva-brown-and-black-dog-posing-for-photo-MADGybfBTkc.jpg",
         scores: [1, 3, 5, 3, 3, 5, 5, 3, 2, 3]
     },
     {
         name: "Bella Block",
-        photo: "https://images.pexels.com/photos/157741/woman-confident-happy-confident-woman-157741.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        photo: "https://marketplace.canva.com/MADGxjjx3OU/4/screen_2x/canva-white-dog-terrier-jumping-near-grass-field-during-daytime-MADGxjjx3OU.jpg",
         scores: [1, 1, 3, 3, 4 ,4, 3, 3, 2, 2]
     },
     {
         name: "McGyver",
-        photo: "https://images.pexels.com/photos/2120084/pexels-photo-2120084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        photo: "https://marketplace.canva.com/MADGvl41ACk/7/screen_2x/canva-american-pit-bull-terrier--MADGvl41ACk.jpg",
         scores: [5, 2, 2, 3, 5, 4, 3, 2, 1, 2]
     },
 
